2574f77593e6a540c6704ace2c65b00c414e2bf7
BrainADZ <web@brainadz.marketing>
2026-07-10 12:43:30 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AboutSection from "@/section/about";
 import BrandLogosCarousel from "@/section/brandslogoSection";
 import CaseStudiesSection from "@/section/caseStudies";
 import FounderDeskSameToSame from "@/section/CeoDesks";
-import CompanyGallerySection from "@/section/companyGallery";
 import ContactCtaSection from "@/section/Ctas";
 import FaqSection from "@/section/faqs";
 import Hero from "@/section/hero";
@@ -10,7 +9,6 @@ import ImpactStartsHereSection from "@/section/ImpactStart";
 import GlobalPresenceSection from "@/section/Maps";
 import ServicesCardsSection from "@/section/serviceSection";
 import TeamShowcaseSection from "@/section/teamShowcase";
-import TechExpertiseSection from "@/section/Technologies";
 import TestimonialSection from "@/section/Testimonials";
 
 export default function Home() {
@@ -25,11 +23,8 @@ export default function Home() {
         <ImpactStartsHereSection/>
         <BrandLogosCarousel />
         <ContactCtaSection/>
-        {/* <WhyDynamicSection/> */}
-        {/* <TechExpertiseSection/> */}
         <FounderDeskSameToSame/>
         <TeamShowcaseSection/>
-        {/* <CompanyGallerySection/> */}
         <TestimonialSection/>
         <FaqSection/>
         <GlobalPresenceSection />
